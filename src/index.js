26c01a329d0fc227c62f3bed49ca52a24109f676
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-02 18:48:21 -0400

    Adding post functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 document.addEventListener("DOMContentLoaded", function(){
     Recipe.getRecipes()
+    mountFormListener()
 })
 
 const recipeList = document.querySelector(".recipe-container")
+const recipeForm = document.querySelector("#new-recipe-form")
+
+function mountFormListener(){
+    recipeForm.addEventListener("submit", function(event){
+        event.preventDefault()
+        console.log("not is being createed")
+        const recipeObj = getRecipeData()
+        Api.post(recipeObj)
+        // debugger
+
+    })
+}
+
+function getRecipeData(){
+    return {
+        title: document.querySelector("#title").value,
+        liquor: document.querySelector("#liquor").value,
+        instructions: document.querySelector("#instructions").value
+    }
+}
+
+function clearForm(){
+    document.querySelector("#title").value = ""
+    document.querySelector("#liquor").value = ""
+    document.querySelector("#instructions").value = ""
+
+}
--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,6 @@
 class Api {
     constructor(){
-        this.baseUrl = 'http://localhost:3000'
+        this.baseUrl = 'http://localhost:3000/recipes'
     }
 
     static get(){
@@ -9,6 +9,38 @@ class Api {
             .then (response => response.json())
         )
         
+    }
+
+    static post(data){
+
+        let configObj = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+            },
+
+            body: JSON.stringify({
+                title: data.title, 
+                liquor: data.liquor, 
+                instructions: data.instructions
+            })
+        }
+
+        fetch("http://localhost:3000/recipes", configObj)
+        .then(response => response.json())
+        .then ((data)=> {
+            if (!data.errors){
+                
+                new Recipe(data)
+                Recipe.renderRecipes()
+                clearForm()
+                debugger
+            } else {
+                throw new Error(`${data.errors}`)
+            }
+        })
+        .catch(alert)
     }
 }
 
