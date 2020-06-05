d8f87cde501253ac4a72d6367ff5cdb8c21595d6
Lyoung0789 <Lestermyoung07@gmail.com>
2020-06-05 17:07:12 -0400

    removing commented out code

--- src/models/reviews.js
+++ src/models/reviews.js
@@ -35,29 +35,9 @@ class Review{
 
     }
 
-    // static createReviewForm(){
-    //     return (`
-    //     <div class ="new-review-form" >
-    //         <form id="new-review-form">
-    //             <div id="form-header"><h2>Add a comment!</h2></div>
-    //             <div class="form-review-name"><input type="text" id="name" placeholder="Your Name"/></div>
-                
-    //             <div class="form-review-content"><textarea name="content" id="content" cols="50" rows="10" placeholder="Add Your Review"></textarea></div>
-    //             <input type="submit"/>
-    //         </form>
-    //     </div>
-        
-    //     `)
-    // }
+
     
 }
 
-// Review.prototype.postHTML = function(){
-//     return (`
-//         <div>
-//             <h4 id="user-name">${this.name}</h4>
-//             <p id="comment">${this.comment}</p>
-//         </div>
-//     `)
-// }
+
 
